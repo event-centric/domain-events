03fbc29f96dd85741b571c02ea0775a079a7b810
Mathias Verraes <mathias@verraes.net>
2014-07-01 21:15:59 +0200

    phpunit + simplified namespace

--- src/EventCentric/DomainEvents/DomainEventsArray.php
+++ src/EventCentric/DomainEvents/DomainEventsArray.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace EventCentric\DomainEvents\Implementations;
+namespace EventCentric\DomainEvents;
 
 use EventCentric\DomainEvents\DomainEvent;
 use EventCentric\DomainEvents\DomainEvents;
